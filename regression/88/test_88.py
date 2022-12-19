87b3462ef26ab77e58e7239c264cb23380a9b16d
Abhishek Jain <abjain39@in.ibm.com>
2022-12-19 06:12:08 -0800

    fourth commit
    Signed-off-by: Abhishek Jain <abjain39@in.ibm.com>

--- regression/88/test_88.py
+++
@@ -1,9 +0,0 @@
-def test_88(kube):
-    d = kube.load_deployment("deployment.yaml")
-
-    d.create()
-    d.wait_until_ready(timeout=30)
-
-    pods = d.get_pods()
-    print(pods)
-    print("hello")
